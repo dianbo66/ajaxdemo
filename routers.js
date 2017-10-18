64c6e5f1ef67cfb0b57caf68a5b2536c5f34d2bd
dianbo66 <dianbostore@163.com>
2017-10-18 09:51:57 +0800

    Create routers.js

---
+++ routers.js
@@ -0,0 +1,16 @@
+router.get('/getNews', function(req, res)){
+	var data = [];
+	for (var i = 0; i <= req.query.pageSize; i++) {
+		data.push("新闻" + parseInt(req.query.page) + i);
+	}
+
+	var info = {
+		status:{
+			statusCode: 1,
+			message: "success"
+		},
+		data: data
+	}
+
+	res.send(info)
+}
